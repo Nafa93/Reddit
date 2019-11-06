87153a9e127083e46515e5ca8d1afe8861f6baf9
Nicolas Alejandro Fernandez Amorosino <namorosino@solstice.com>
2019-11-05 22:25:23 -0300

    - Added PostTableViewCell to display posts

---
+++ Reddit/Reddit/Cells/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Reddit
+//
+//  Created by Nicolas Fernandez Amorosino on 05/11/2019.
+//  Copyright © 2019 Nicolas Amorosino. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
