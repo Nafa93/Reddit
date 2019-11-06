44711e4363eda9c65cba560e52930841a280fa4f
Nicolas Alejandro Fernandez Amorosino <namorosino@solstice.com>
2019-11-05 21:17:51 -0300

    - Added PostViewModel to handle all API calls, data parsing and transformation

---
+++ Reddit/Reddit/ViewModels/PostsViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  PostsViewModel.swift
+//  Reddit
+//
+//  Created by Nicolas Fernandez Amorosino on 05/11/2019.
+//  Copyright © 2019 Nicolas Amorosino. All rights reserved.
+//
+
+import Foundation
+
+class PostsViewModel {
+    init() {
+
+    }
+
+    
+}
